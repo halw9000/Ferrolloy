dd6944bc7d75aec3e2fbc6cbca8c5803aab75715
Hal <halmccoy3@gmail.com>
2025-01-20 13:26:45 -0600

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,11 +2,15 @@ import pandas as pd
 import streamlit as st
 
 
-uploaded_file = st.file_uploader("Upload Excel Jobs File Here")
+uploaded_file = st.file_uploader("Upload Excel Jobs File Here", type={"xlsx"})
 
 if uploaded_file is not None:
-   df1=pd.read_excel(uploaded_file)
-   filename=uploaded_file.name
+   xls = pd.ExcelFile(uploaded_file)
+   sheet_names = xls.sheet_names
+   with st.form(key='sheet_selector_form'):
+        selected_sheet = st.selectbox("Select a sheet to load data", sheet_names)
+        selected_skip_lines = st.text_input("Number of lines to skip (including header if present)")
+        submit_button = st.form_submit_button(label='Submit')
 else:
    st.warning("")
 
