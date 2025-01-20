ee0f2743c776bd017603b5fe3ce5ea1ffa25a116
Hal <halmccoy3@gmail.com>
2025-01-20 13:09:09 -0600

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,4 +1,15 @@
 import pandas as pd
 import streamlit as st
 
-st.write("Hello Streamlit")
+uploaded_file = st.file_uploader(“Choose a file”)
+
+if uploaded_file is not None:
+#read csv
+df1=pd.read_csv(uploaded_file)
+
+   #read xls or xlsx
+   df1=pd.read_excel(uploaded_file)
+else:
+st.warning(“you need to upload a csv or excel file.”)
+
+filename=uploaded_file.name
