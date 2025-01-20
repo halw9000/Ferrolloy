b10906b904013dc0b99e662136a99686782639ea
Hal <halmccoy3@gmail.com>
2025-01-20 13:13:10 -0600

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import streamlit as st
 
-uploaded_file = st.file_uploader("")
+uploaded_file = st.file_uploader("Updload Jobs Here")
 
 if uploaded_file is not None:
    #read csv
    df1=pd.read_csv(uploaded_file)
    #read xls or xlsx
    df1=pd.read_excel(uploaded_file)
+   filename=uploaded_file.name
 else:
    st.warning("")
 
