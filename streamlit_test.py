8557a3a7922326cb76fd2775c43024f1a172d42a
Hal <halmccoy3@gmail.com>
2025-01-20 13:11:15 -0600

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -4,9 +4,8 @@ import streamlit as st
 uploaded_file = st.file_uploader()
 
 if uploaded_file is not None:
-#read csv
-df1=pd.read_csv(uploaded_file)
-
+   #read csv
+   df1=pd.read_csv(uploaded_file)
    #read xls or xlsx
    df1=pd.read_excel(uploaded_file)
 else:
