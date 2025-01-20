e9fae12d84908d1ad40ac9712ac82bed755d8aac
Hal <halmccoy3@gmail.com>
2025-01-20 14:06:02 -0600

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -18,17 +18,20 @@ if uploaded_file is not None:
          with st.spinner('Generating schedules for: ' + selected_iron):
             time.sleep(2)
          st.success("Done!")
-         with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-       # Write each dataframe to a different worksheet.
-             df_jobs.to_excel(writer, sheet_name='Sheet1')
-          # Close the Pandas Excel writer and output the Excel file to the buffer
-             writer.close()
-             st.download_button(
-                 label="Download Excel worksheets",
-                 data=buffer,
-                 file_name="pandas_multiple.xlsx",
-                 mime="application/vnd.ms-excel"
-             )
+         to_download == 1
+         break
+   while to_download == 1:
+      with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+    # Write each dataframe to a different worksheet.
+          df_jobs.to_excel(writer, sheet_name='Sheet1')
+       # Close the Pandas Excel writer and output the Excel file to the buffer
+          writer.close()
+          st.download_button(
+              label="Download Excel worksheets",
+              data=buffer,
+              file_name="pandas_multiple.xlsx",
+              mime="application/vnd.ms-excel"
+          )
          
 
 
