f1e9e5fc03d61e1fc58ed178825f5c7409f25b0d
Hal <halmccoy3@gmail.com>
2025-01-20 21:37:46 -0600

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -8,10 +8,7 @@ def schedule_info(df):
     info = "Pour Weight: " + str(df['pour_wt'].sum()) + ",  Total Molds: " + str(df['mold_qty'].sum()) + ",  Mold Hours: " + str(round(df['mold_hrs'].sum(),1)) + ",  Pattern Switches: " + str(len(df)-1)
     return info
     
-st.set_page_config(
-    page_title="FAKE Scheduler",
-    page_icon="🛠",
-)
+
 
 st.sidebar.header("Ferroloy")
 
