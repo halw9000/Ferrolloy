360ced8e9bb6ff6f5bec817b61fcfc65e19fd6db
Hal <halmccoy3@gmail.com>
2025-01-21 15:20:12 -0600

    Update fdnx_compare.py

--- fdnx_compare.py
+++ fdnx_compare.py
@@ -15,7 +15,7 @@ st.write("Upload additional FDNX schedule files to simulate and compare.")
 
 col1, col2 = st.columns(2)
 with col1:
-  com_file1 = st.file_uploader("Upload FDNX Schedule 1", type={"xlsx"})
+  com_file1 = st.file_uploader("Upload FDNX Schedule A", type={"xlsx"})
   if com_file1 is not None:
     is_comparing = 1
     com1_fdnx1 = pd.read_excel(com_file1, sheet_name="FDNX1")
@@ -24,28 +24,55 @@ with col1:
     com1_schedule = [com1_fdnx1,com1_fdnx2,com1_fdnx3]
     with st.spinner("Simulating..."):
         com1_ladles, com1_lanes, com1_sim_seconds = fx.fdnx_simulator(com1_schedule)   
-    st.write("Schedule 1: ")
-    st.write("Ladles: " + str(len(com1_ladles)))
+    st.write("Schedule A: " + str(len(com1_ladles))) + " ladles")
     
     ## update Charts
     com1_mold_wt_chart_data = com1_ladles[['ladle_number', 'total_mold_wt']]
-    com1_mold_wt_chart_data.rename(columns={'ladle_number': 'ladle_number', 'total_mold_wt': 'Schedule A'}, inplace=True)
-    new_mold_wt_chart_data = st.session_state.mold_wt_chart_data.merge(com1_mold_wt_chart_data,on="ladle_number",how="outer")
-    st.session_state.mold_wt_chart_data = new_mold_wt_chart_data
-
     com1_mold_count_chart_data = com1_ladles[['ladle_number', 'molds_filled']]
-    com1_mold_count_chart_data.rename(columns={'ladle_number': 'ladle_number', 'molds_filled': 'Schedule A'}, inplace=True)
-    new_mold_count_chart_data = st.session_state.mold_count_chart_data.merge(com1_mold_count_chart_data,on="ladle_number",how="outer")
-    st.session_state.mold_count_chart_data = new_mold_count_chart_data
-
     com1_mold_avgwt_chart_data = com1_ladles[['ladle_number', 'avg_mold_wt']]
+    
+    com1_mold_wt_chart_data.rename(columns={'ladle_number': 'ladle_number', 'total_mold_wt': 'Schedule A'}, inplace=True)
+    com1_mold_count_chart_data.rename(columns={'ladle_number': 'ladle_number', 'molds_filled': 'Schedule A'}, inplace=True)
     com1_mold_avgwt_chart_data.rename(columns={'ladle_number': 'ladle_number', 'avg_mold_wt': 'Schedule A'}, inplace=True)
+    
+    new_mold_wt_chart_data = st.session_state.mold_wt_chart_data.merge(com1_mold_wt_chart_data,on="ladle_number",how="outer")
+    new_mold_count_chart_data = st.session_state.mold_count_chart_data.merge(com1_mold_count_chart_data,on="ladle_number",how="outer")
     new_mold_avgwt_chart_data = st.session_state.mold_avgwt_chart_data.merge(com1_mold_avgwt_chart_data,on="ladle_number",how="outer")
+    
+    st.session_state.mold_wt_chart_data = new_mold_wt_chart_data
+    st.session_state.mold_count_chart_data = new_mold_count_chart_data
     st.session_state.mold_avgwt_chart_data = new_mold_avgwt_chart_data
     
 with col2:
-  com_file2 = st.file_uploader("Upload FDNX Schedule 2", type={"xlsx"})
+  com_file2 = st.file_uploader("Upload FDNX Schedule B", type={"xlsx"})
+  if com_file1 is not None:
+    is_comparing = 1
+    com2_fdnx1 = pd.read_excel(com_file2, sheet_name="FDNX1")
+    com2_fdnx2 = pd.read_excel(com_file2, sheet_name="FDNX2")
+    com2_fdnx3 = pd.read_excel(com_file2, sheet_name="FDNX3")
+    com2_schedule = [com2_fdnx1,com2_fdnx2,com2_fdnx3]
+    with st.spinner("Simulating..."):
+        com2_ladles, com2_lanes, com2_sim_seconds = fx.fdnx_simulator(com2_schedule)   
+      
+    st.write("Schedule B: " + str(len(com2_ladles))) + " ladles")
+    
+    ## UPDATE CHARTS
 
+    com2_mold_wt_chart_data = com2_ladles[['ladle_number', 'total_mold_wt']]
+    com2_mold_count_chart_data = com2_ladles[['ladle_number', 'molds_filled']]
+    com2_mold_avgwt_chart_data = com2_ladles[['ladle_number', 'avg_mold_wt']]
+    
+    com2_mold_wt_chart_data.rename(columns={'ladle_number': 'ladle_number', 'total_mold_wt': 'Schedule B'}, inplace=True)
+    com2_mold_count_chart_data.rename(columns={'ladle_number': 'ladle_number', 'molds_filled': 'Schedule B'}, inplace=True)
+    com2_mold_avgwt_chart_data.rename(columns={'ladle_number': 'ladle_number', 'avg_mold_wt': 'Schedule B'}, inplace=True)
+    
+    new_mold_wt_chart_data = st.session_state.mold_wt_chart_data.merge(com2_mold_wt_chart_data,on="ladle_number",how="outer")
+    new_mold_count_chart_data = st.session_state.mold_count_chart_data.merge(com2_mold_count_chart_data,on="ladle_number",how="outer")
+    new_mold_avgwt_chart_data = st.session_state.mold_avgwt_chart_data.merge(com2_mold_avgwt_chart_data,on="ladle_number",how="outer")
+    
+    st.session_state.mold_wt_chart_data = new_mold_wt_chart_data
+    st.session_state.mold_count_chart_data = new_mold_count_chart_data
+    st.session_state.mold_avgwt_chart_data = new_mold_avgwt_chart_data
 
 
 if 'mold_wt_chart_data' in st.session_state:
@@ -83,14 +110,6 @@ else:
     mold_avgwt_chart_data.rename(columns={'ladle_number': 'ladle_number', 'avg_mold_wt': 'Simulated'}, inplace=True)
     st.session_state.mold_avgwt_chart_data = mold_avgwt_chart_data
     
-  
-  
-    #CHARTS
-
-    #st.header("Molds Filled Per Ladle:")
-    #st.line_chart(mold_count_chart_data, x="ladle_number",y="molds_filled")
-    #st.header("Average Pour Weight:")
-    #st.line_chart(mold_avgwt_chart_data, x="ladle_number",y="avg_mold_wt")
 
 if to_download == 1:
     with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
