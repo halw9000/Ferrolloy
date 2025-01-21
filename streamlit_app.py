d38e98c6f56e8ceb654abd993d4fdcb2a04a600d
Hal <halmccoy3@gmail.com>
2025-01-21 12:26:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-fdnx_upload = st.Page("fdnx_upload.py", title="Upload and Simulate", icon=":material/add_circle:")
-fdnx_compare = st.Page("fdnx_compare.py", title="Compare Schedules", icon=":material/delete:")
+fdnx_upload = st.Page("fdnx_upload.py", title="Upload and Simulate", icon="🛠")
+fdnx_compare = st.Page("fdnx_compare.py", title="Compare Schedules", icon="🎬")
 
 pg = st.navigation([fdnx_upload, fdnx_compare])
 
