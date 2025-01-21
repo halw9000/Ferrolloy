a72e1cd58d4112d79d2f75638dca3f87afa0a9d6
Hal <halmccoy3@gmail.com>
2025-01-20 21:30:52 -0600

    Update fdnx_home.py

--- fdnx_home.py
+++ fdnx_home.py
@@ -15,7 +15,8 @@ st.set_page_config(
 
 st.navigation([
     st.Page('fdnx_home.py', title='FDNX Scheduler', icon='⏰'),
-    st.Page('pages/simulation.py', title='Deck Simulator', icon='🧰️')
+    st.Page('pages/simulation.py', title='Deck Simulator', icon='🧰️'),
+    st.Page('streamlit_test.py', title='test page', icon='🧰️')
 ])
 
 
