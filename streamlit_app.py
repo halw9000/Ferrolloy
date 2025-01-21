f5c66a2c9147123f46e175bfc55913a582bb897c
Hal <halmccoy3@gmail.com>
2025-01-21 15:51:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 fdnx_upload = st.Page("fdnx_upload.py", title="Upload and Simulate", icon="🛠")
 fdnx_compare = st.Page("fdnx_compare.py", title="Compare Schedules", icon="🎬")
+fdnx_about = st.Page("fdnx_about.py", title="About", icon="💡")
 
-pg = st.navigation([fdnx_upload, fdnx_compare])
+pg = st.navigation([fdnx_upload, fdnx_compare, fdnx_about])
 
 st.set_page_config(page_title="Ferroloy Scheduler", page_icon="🛠")
 
