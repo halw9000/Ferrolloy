592b039fee4cab9a64c9d0932d904d2c972f311f
Hal <halmccoy3@gmail.com>
2025-01-21 09:58:18 -0600

    Update fdnx_simulator.py

--- fdnx_simulator.py
+++ fdnx_simulator.py
@@ -172,7 +172,18 @@ def fdnx_simulator(test_schedule):
     lane_6 = FDNX_3_lanes[1]
 
     # Initialize ladles DataFrame
-    ladles = pd.DataFrame(columns=['ladle_number', 'ladle_weight', 'ladle_start_weight', 'ladle_temp', 'ladle_start_temp', 'start_time', 'molds_filled', 'total_mold_wt', 'end_time'])
+    ladle_dytpe = {
+        'ladle_number': int,  
+        'ladle_weight': float,
+        'ladle_start_weight': float,  
+        'ladle_temp': float,
+        'ladle_start_temp': float,
+        'start_time': float,
+        'molds_filled': float,
+        'total_mold_wt': float,
+        'end_time': float
+    }
+    ladles = pd.DataFrame(dtype=ladle_dtype)
     current_time = 0
     ladle_number = 1
     # Create the first ladle
