303056d3b6c315d576eceb64f1d97ff61f92796c
Hal <halmccoy3@gmail.com>
2025-01-20 20:52:32 -0600

    Update 1_📑_Simulation.py

--- pages/1_📑_Simulation.py
+++ pages/1_📑_Simulation.py
@@ -4,6 +4,8 @@ import numpy as np
 import time, io, datetime
 import fdnx_scheduler as fs
 
+st.set_page_config(page_title="Deck Simulator", page_icon="📑")
+
 st.title("simulator")
 
 st.write("TEST")
