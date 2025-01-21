cbc46e153a71d0818b34916918a2b7f7be0ae487
Hal <halmccoy3@gmail.com>
2025-01-20 23:08:51 -0600

    Update fdnx_home.py

--- fdnx_home.py
+++ fdnx_home.py
@@ -72,14 +72,14 @@ if uploaded_file is not None:
                 mime="application/vnd.ms-excel"
             )
                 
-            with st.form(key='simulator_form'): 
-                simulate_button = st.form_submit_button(label='Simulate This Schedule')
-                if simulate_button:
-                    with st.spinner("Simulation running. Should take ~30s"):
-                        st.write("simulated")
-                        #ladles, lanes, sim_seconds = fx.fdnx_simulator(schedules_made)
-                    #st.header("Ladles Table:")
-                    #st.dataframe(ladles)
+    with st.form(key='simulator_form'): 
+    simulate_button = st.form_submit_button(label='Simulate This Schedule')
+    if simulate_button:
+        with st.spinner("Simulation running. Should take ~30s"):
+            st.write("simulated")
+            ladles, lanes, sim_seconds = fx.fdnx_simulator(schedules_made)
+        st.header("Ladles Table:")
+        st.dataframe(ladles)
                 
 
 
