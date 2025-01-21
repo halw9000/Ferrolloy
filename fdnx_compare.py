00fb408323b62582771f5984eb157815dc14b9bd
Hal <halmccoy3@gmail.com>
2025-01-21 12:38:15 -0600

    Update fdnx_compare.py

--- fdnx_compare.py
+++ fdnx_compare.py
@@ -5,11 +5,11 @@ import time, io, datetime
 import fdnx_simulator as fx
 import fdnx_scheduler as fs
 
-col1, col2, col3 = st.columns(3)
-
 st.write("# Compare Schedules Here")
 st.write("Upload up to three FDNX schedule files to simulate and compare.")
 
+
+col1, col2, col3 = st.columns(3)
 with col1:
   com_file1 = st.file_uploader("Upload FDNX Schedule 1", type={"xlsx"})
 
