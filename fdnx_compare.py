f69f6dd3c7537c85b9f86a398ba541ce08d4f621
Hal <halmccoy3@gmail.com>
2025-01-21 12:36:03 -0600

    Update fdnx_compare.py

--- fdnx_compare.py
+++ fdnx_compare.py
@@ -1,3 +1,20 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import time, io, datetime
+import fdnx_simulator as fx
+import fdnx_scheduler as fs
+
+col1, col2, col3 = st.columns(3)
 
 st.write("# Compare Schedules Here")
+st.write("Upload up to three FDNX schedule files to simulate and compare.")
+
+with col1:
+  com_file1 = st.file_uploader("Upload FDNX Schedule 1", type={"xlsx"})
+
+with col2:
+  com_file1 = st.file_uploader("Upload FDNX Schedule 2", type={"xlsx"})
+
+with col3:
+  com_file1 = st.file_uploader("Upload FDNX Schedule 3", type={"xlsx"})
