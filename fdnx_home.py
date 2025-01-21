c2ce2f8077b933ea00ead71d7e8d66ae3d0c5ff1
Hal <halmccoy3@gmail.com>
2025-01-20 22:52:06 -0600

    Update fdnx_home.py

--- fdnx_home.py
+++ fdnx_home.py
@@ -53,12 +53,7 @@ if uploaded_file is not None:
             st.write(schedule_info(fdnx3))
             st.dataframe(fdnx3)
             to_download = 1
-            simulate_button = st.form_submit_button(label='Simulate This Schedule')
-            if simulate_button:
-                        with st.spinner("Simulation running. Should take ~30s"):
-                            ladles, lanes, sim_seconds = fx.fdnx_simulator(schedules_made)
-                        st.header("Ladles Table:")
-                        st.dataframe(ladles)
+
     if to_download == 1:
         with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
         # Write each dataframe to a different worksheet.
@@ -75,8 +70,14 @@ if uploaded_file is not None:
                 file_name= "FDNXSchedule_" + timestamp +".xlsx",
                 mime="application/vnd.ms-excel"
               )
-
-
+        with st.form(key='simulator_form'):
+            simulate_button = st.form_submit_button(label='Simulate This Schedule')
+            if simulate_button:
+                with st.spinner("Simulation running. Should take ~30s"):
+                    ladles, lanes, sim_seconds = fx.fdnx_simulator(schedules_made)
+                st.header("Ladles Table:")
+                st.dataframe(ladles)
+                
 
 
 #chart_data1 = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
