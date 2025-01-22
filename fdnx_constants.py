650c30415062d50e9a68a2e665c29c8139480d79
Hal <halmccoy3@gmail.com>
2025-01-22 14:40:40 -0600

    Update fdnx_constants.py

--- fdnx_constants.py
+++ fdnx_constants.py
@@ -13,7 +13,7 @@ deck_lanes = 6
 lane_cart_capacity = 20  # Capacity of each lane in terms of carts (not needed???)
 molds_per_cart = 3  # Number of molds per cart
 pour_speed_lbs_sec = 2  # Pour speed in lbs/sec
-cart_pour_buffer_sec = 10  # Buffer time for pouring carts
+cart_pour_buffer_sec = 15  # Buffer time for pouring carts
 distance_between_lanes_sec = 10  # Distance between lanes in seconds
 
 ## LADLE Constants
